122dbed6eb91d489bf393ac7f350dcbdc4cccad0
İlkay Şeki <sekiilkay@gmail.com>
2023-10-12 14:06:38 +0300

    Create Repository

---
+++ JwtUser.API/JwtUser.Core/Services/IHelpDemandService.cs
@@ -0,0 +1,16 @@
+﻿using JwtUser.Core.DTOs.Request;
+using JwtUser.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtUser.Core.Services
+{
+    public interface IHelpDemandService : IGenericService<HelpDemand>
+    {
+        public Task AddHelpDemand(AddHelpDemandDto helpDemand);
+
+    }
+}
